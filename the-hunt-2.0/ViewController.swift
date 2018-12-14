09664e35a9c9977a5179a48d50895a40f702197d
John Maddock <jmmaddock@gmail.com>
2018-12-14 14:09:09 -0700

    Initial Commit

---
+++ the-hunt-2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  the-hunt-2.0
+//
+//  Created by John Maddock on 12/14/18.
+//  Copyright © 2018 John Maddock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
